a5549fcba13db19426c7e7c550db62207603fce5
nillk <hyunjoo88.kim@gmail.com>
2019-09-26 01:36:02 +0900

    Add space after theater brand name

--- src/components/theater/index.js
+++ src/components/theater/index.js
@@ -2,9 +2,22 @@ import React from "react"
 
 import style from "./index.module.css"
 
+const addSpaceAfterBrandName = (name) => {
+  const check = /(CGV|메가박스|롯데시네마)(?!\s)/.exec(name)
+
+  if (check !== null) {
+    const length = check[0].length
+    const index = check["index"]
+
+    return name.slice(0, length + index) + " " + name.slice(length + index)
+  }
+
+  return name
+}
+
 const Theater = ({ name }) => (
     <div className={style.theater}>
-      {name}
+      {addSpaceAfterBrandName(name)}
     </div>
 );
 
